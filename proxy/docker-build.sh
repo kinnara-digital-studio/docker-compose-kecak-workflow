8d23874db9fce0e0f8eeefe6c63db5f62061badf
aristosh <aristo.hadisoeganda@gmail.com>
2019-09-13 20:50:07 +0700

    add wait-for-it script

--- proxy/docker-build.sh
+++ proxy/docker-build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-docker build -t kinnaradigitalstudio/kecak-proxy:no-ssl .
+if [ -z "$1" ]
+then
+  TAG=latest
+else
+  TAG=$1
+fi
+
+docker build -t kinnaradigitalstudio/kecak-proxy:no-ssl-$TAG .
